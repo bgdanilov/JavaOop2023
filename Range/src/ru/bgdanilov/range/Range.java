8ec6180907c6712ad7990733a9617dfde6658ac3
bgdanilov <bdan@yandex.ru>
2023-07-21 13:59:21 +0700

    Range project from Osnovi Java

--- Range/src/ru/bgdanilov/range/Range.java
+++ Range/src/ru/bgdanilov/range/Range.java
@@ -1,4 +1,47 @@
 package ru.bgdanilov.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    // Конструктор.
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    // Геттеры - сеттеры.
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    // Методы:
+    // Длина диапазона.
+    public double getLength() {
+        return to - from;
+    }
+
+    // Середина диапазона.
+    public double getMiddle() {
+        return (to - from) / 2 + from;
+    }
+
+    // Принадлежит ли число диапазону?
+    public boolean isInside(double number) {
+        double epsilon = 1.0e-10;
+
+        return number - from >= -epsilon && to - number >= -epsilon;
+    }
 }
--- Range/src/ru/bgdanilov/range_main/Main.java
+++ Range/src/ru/bgdanilov/range_main/Main.java
@@ -1,7 +1,55 @@
 package ru.bgdanilov.range_main;
 
+import ru.bgdanilov.range.Range;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Создаем объект класса.
+        Range myRange = new Range(2.34, 9.76);
+
+        System.out.printf("Диапазон: от %.3f до %.3f.%n", myRange.getFrom(), myRange.getTo());
+        System.out.printf("Длина диапазона: %.3f.%n", myRange.getLength());
+        System.out.println("-----------");
+
+        myRange.setFrom(-3); // Меняем поле from.
+        myRange.setTo(1); // Меняем поле to.
+        System.out.printf("Измененный диапазон: от %.3f до %.3f.%n", myRange.getFrom(), myRange.getTo());
+
+        // Проверяем, лежит ли число в диапазоне.
+        double number = -3;
+        System.out.printf("Лежит ли число %.3f в диапазоне? %b%n", number, myRange.isInside(number));
+        System.out.println("-----------");
+
+        // Программа. Какое из двух чисел лежит ближе к середине диапазона?
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первое число: ");
+        double number1 = scanner.nextDouble();
+
+        System.out.print("Введите второе число: ");
+        double number2 = scanner.nextDouble();
+
+        System.out.println("-----------");
+
+        // Ищем середину диапазона.
+        double rangeMiddle = myRange.getMiddle();
+        System.out.printf("Середина диапазона: %.3f.%n", rangeMiddle);
+
+        double epsilon = 1.0e-10;
+        boolean isNumber1Inside = myRange.isInside(number1);
+        boolean isNumber2Inside = myRange.isInside(number2);
+
+        if (isNumber1Inside || isNumber2Inside) {
+            if ((Math.abs(rangeMiddle - number1) - Math.abs(rangeMiddle - number2)) > epsilon) {
+                System.out.printf("Число %.3f ближе к середине диапазона.%n", number2);
+            } else if ((Math.abs(rangeMiddle - number2) - Math.abs(rangeMiddle - number1)) > epsilon) {
+                System.out.printf("Число %.3f ближе к середине диапазона.%n", number1);
+            } else {
+                System.out.printf("Оба числа %.3f и %.3f равноудалены от середины диапазона.%n", number1, number2);
+            }
+        } else {
+            System.out.printf("Оба числа %.3f и %.3f не лежат в диапазоне.%n", number1, number2);
+        }
     }
 }
