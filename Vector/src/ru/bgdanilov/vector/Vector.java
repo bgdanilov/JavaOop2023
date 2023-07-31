3b7deab71d4e4454bf6d9df5d4fe72ecd69322ef
borisdanilov <bdan@yandex.ru>
2023-07-31 09:13:45 +0700

    Add Vector.

--- Vector/src/ru/bgdanilov/vector/Vector.java
+++ Vector/src/ru/bgdanilov/vector/Vector.java
@@ -3,19 +3,17 @@ package ru.bgdanilov.vector;
 import java.util.Arrays;
 
 public class Vector {
-    // У вектора есть единственное поле - массив его координат.
     private double[] components;
 
-    //// Конструкторы с перегрузкой:
-    // a. Конструктор - создание пустого вектора размерностью n.
-    public Vector(int n) {
+    // 1.a. Конструктор - создание пустого вектора размерностью n.
+    public Vector(int size) {
         try {
             // Если n < 0 - бросаем исключение.
-            if (n <= 0) {
+            if (size <= 0) {
                 throw new IllegalArgumentException();
             }
             // Код, выполняемый в отсутствии исключения.
-            this.components = new double[n];
+            this.components = new double[size];
         }
         // Обрабатываем исключение.
         catch (IllegalArgumentException e) {
@@ -23,13 +21,13 @@ public class Vector {
         }
     }
 
-    // b. Конструктор - копирование из другого вектора.
+    // 1.b. Конструктор - копирование из другого вектора.
     public Vector(Vector vector) {
-        int n = vector.getSize(); // Размер вектора-источника.
-        this.components = Arrays.copyOf(vector.components, n);
+        int size = vector.getSize(); // Размер вектора-источника.
+        this.components = Arrays.copyOf(vector.components, size);
     }
 
-    // c. Конструктор - заполнение вектора значениями из массива.
+    // 1.c. Конструктор - заполнение вектора значениями из массива.
     public Vector(double[] components) {
         try {
             if (components.length == 0) {
@@ -42,37 +40,172 @@ public class Vector {
         }
     }
 
-    // d. Конструктор - заполнение вектора значениями из массива.
-    // Если длины массивов не совпадают,
-    // то результирующий массив обрежется или заполнится нулями.
-    public Vector(int n, double[] array) {
+    // 1.d. Конструктор - заполнение вектора значениями из массива.
+    // Если длина массива меньше n, то считать что в остальных компонентах 0.
+    public Vector(int size, double[] array) {
         try {
-            if (n <= 0 || array.length == 0) {
+            if (size <= 0 || array.length == 0) {
                 throw new IllegalArgumentException();
             }
 
-            this.components = Arrays.copyOf(array, n);
+            this.components = Arrays.copyOf(array, size);
         } catch (IllegalArgumentException e) {
             System.out.println("n must be > 0");
         }
     }
 
-
-    //// Методы:
-    // Получение размерности вектора - по-сути длину массива координат.
-    // Своего рода - геттер.
+    // 2. Метод. Получение размерности вектора - по-сути длину массива компонент.
     public int getSize() {
         return this.components.length;
     }
 
-    // Задание компонент существующего вектора вручную.
-    // Сеттер.
+    // 3. Метод. toString(), чтобы выдавал информацию о векторе в  формате { значения компонент через запятую }
+    @Override
+    public String toString() {
+        int size = this.getSize();
+        StringBuilder sb = new StringBuilder().append("{ ").append(this.components[0]);
+
+        for (int i = 1; i < size; i++) {
+            sb.append(", ").append(components[i]);
+        }
+
+        return sb.append(" }").toString();
+    }
+
+    // 4.a. Добавить вектор к вектору.
+    public void addVector(Vector additionalVector) {
+        int sourceVectorSize = this.getSize();
+        int additionalVectorSize = additionalVector.getSize();
+
+        if (additionalVectorSize > sourceVectorSize) {
+            this.components = Arrays.copyOf(additionalVector.components, additionalVectorSize);
+        }
+
+        for (int i = 0; i < sourceVectorSize; i++) {
+            this.components[i] += additionalVector.components[i];
+        }
+    }
+
+    // 4.b. Вычесть вектор из вектора.
+    public void subtractVector(Vector deductedVector) {
+        int deductedVectorSize = deductedVector.getSize();
+        int sourceVectorSize = this.getSize();
+
+        // Добавочный вектор дополним нулями до размерности исходного.
+        if (deductedVectorSize < sourceVectorSize) {
+            deductedVector.components = Arrays.copyOf(deductedVector.components, sourceVectorSize);
+        }
+
+        for (int i = 0; i < sourceVectorSize; i++) {
+            this.components[i] -= deductedVector.components[i];
+        }
+    }
+
+    // 4.c. Умножение вектора на скаляр.
+    public void multiplicationByScalar(double scalar) {
+        int vectorSize = this.getSize();
+
+        for (int i = 0; i < vectorSize; i++) {
+            this.components[i] *= scalar;
+        }
+    }
+
+    // 4.d. Разворот вектора. Умножение на -1.
+    public void reverseVector() {
+        this.multiplicationByScalar(-1);
+    }
+
+    // 4.e. Получение длины веектора.
+    public double getLength() {
+        int size = this.getSize();
+        double sqrtExpression = 0;
+
+        for (int i = 0; i < size; i++) {
+            sqrtExpression += Math.pow(this.components[i], 2);
+        }
+
+        return Math.sqrt(sqrtExpression);
+    }
+
+    // 4.f. Получение и установка компоненты вектора по индексу.
+    public double getComponent(int index) {
+        return this.components[index];
+    }
+
+    public void setComponent(int index, double component) {
+        this.components[index] = component;
+    }
+
+    // 5.a. Сложение двух векторов.
+    public static Vector additionVectors(Vector vector1, Vector vector2) {
+        Vector resultVector = new Vector(vector1);
+        resultVector.addVector(vector2);
+
+        return resultVector;
+    }
+
+    // 5.b. Вычитание двух векторов.
+    public static Vector subtractionVectors(Vector vector1, Vector vector2) {
+        Vector resultVector = new Vector(vector1);
+        resultVector.subtractVector(vector2);
+
+        return resultVector;
+    }
+
+    // 5.c. Скалярное произведение двух векторов.
+    public static double dotProductVectors(Vector vector1, Vector vector2) {
+        int size = Math.min(vector1.getSize(), vector2.getSize());
+        double dotProduct = 0;
+
+        for (int i = 0; i < size; i++) {
+            dotProduct += vector1.components[i] * vector2.components[i];
+        }
+
+        return dotProduct;
+    }
+
+    // Метод. Задание компонент существующего вектора вручную.
     public void setComponents(double[] components) {
         this.components = components;
     }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(this.components);
-    }
 }
+
+/*  Описание класса:
+    ===============================
+    - У вектора есть единственное поле - массив его компонент.
+    - Конструкторы - функции с перегрузкой.
+
+    1.d. Конструктор - заполнение вектора значениями из массива.
+     - Если компоненты не поместились (длина меньше длины существующего вектора), то лишние значения игнорируются.
+     - Если длина массива больше длины вектора, ячейки заполняются нулями.
+
+    4.a. Метод "Добавить вектор к вектору".
+    - Значит к компонентам исходного вектора прибавить соответствующие компоненты добавляемого вектора.
+    - Мы движемся в цикле по добавочному вектору, потому, что его компоненты добавляем  к компонентам исходника.
+    - Поэтому, если добавочный по размеру превышает исходник, то его компоненты все не влезут.
+    - Нужно дополнить исходник до размеров добавочного.
+    - А если наоборот, то меньшее всегда влезет в большее.
+    - К компонентам исходника, которые за пределами размерности бобавочного вектора, просто ничего не прибавится,
+      что равносзначно прибавлению нуля. Поэтому дополнять нулями добавочный вектор до размеров исходного необязательно.
+            source:     [1, 2, 3]     или  [1, 2, 3, 4]
+          + additional: [1, 2, 3, 4]  или  [1, 2, 3]
+          = source:     [2, 4, 6, 4]       [2, 4, 6, 4]
+
+    4.b. Метод "Вычесть вектор из вектора".
+    - Аналогично методу "Добавить вектор к вектору".
+
+    5.c. Метод "Скалярное произведение двух векторов".
+    - Скалярным произведением двух векторов называется сумма произведений соответствующих компонент векторов.
+            vector1:    [1, 2, 3, 4]         или  [1, 2, 3]
+          + vector2:    [1, 2, 3]            или  [1, 2, 3, 4]
+          = dotProduct:  1 + 4 + 9 + 4 * 0         1 + 4 + 9
+          Если число итераций цикла будет равно большей размерности из двух вектров,
+          то надо дополнять меньший вектор нулями.
+          а если идти итерировать по меньшей размерности, то просто четверку ни на что не умножаем,
+          и нулями заполнять не нужно.
+          Можно так же, наверное, поступить в 4.a. ?
+ */
+
+/* TODO
+    1. Проверить возможность реализации 4.a как в 5.c : через Math.min .
+ */
--- Vector/src/ru/bgdanilov/vector_main/VectorMain.java
+++ Vector/src/ru/bgdanilov/vector_main/VectorMain.java
@@ -4,6 +4,45 @@ import ru.bgdanilov.vector.Vector;
 
 public class VectorMain {
     public static void main(String[] args) {
+        // 1. Конструкторы.
+        // -------------------------
+        // 1.a. Создаем нулевой вектор размерностью 10.
+        Vector vectorA = new Vector(10);
+        System.out.println("Вектор A: " + vectorA.toString());
+
+        // 1.c. Создаем вектор из значений массива.
+        Vector vectorC = new Vector(new double[] {1, 2, 3, 4, 5});
+        System.out.println("Вектор C: " + vectorC.toString());
+
+        // 1.b. Создаем вектор копированием из другого вектора.
+        Vector vectorB = new Vector(vectorC);
+        System.out.println("Вектор B: " + vectorB.toString());
+
+        // 1.d. Создаем вектор и производим заполнение вектора значениями из массива.
+        Vector vectorD = new Vector(10, new double[] {1, 2, 3, 4, 5});
+        System.out.println("Вектор D: " + vectorD.toString());
+
+        // 2. Метод getSize() для получения размерности вектора
+        System.out.println("Размер вектора D: " + vectorD.getSize());
+
+        // 3. Реализовать метод toString() - проверено выше.
+        System.out.println();
+
+        // 4. Реализовать  нестатические методы:
+        // ---------------------------------------
+        System.out.println("4. Реализовать нестатические методы:");
+        // 4.a. Прибавление к вектору другого вектора.
+        vectorB.addVector(vectorD);
+        System.out.println("4.a. К Вектору B добавляем Вектор D: " + vectorB.toString());
+
+
+
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        /*
         // Конструктор a. - создаем пустой массив.
         Vector vectorA = new Vector(10);
         // Допустим, я в конструктор выше передам n = 0.
@@ -12,27 +51,46 @@ public class VectorMain {
         // но объект vectorA все равно создастся.
         // И потом все равно будет ошибка при попытке вызова vector.getSize()
         // Не должен был быть объект создаться же?
-        vectorA.setComponents(new double[]{1, 2, 2, 2});
+        vectorA.setComponents(new double[] {1, 2, 2, 2});
 
-        System.out.println(vectorA.toString());
+        System.out.println("Вектор A: " + vectorA.toString());
         System.out.println(vectorA.getSize());
 
         // Конструктор b. - копирование из другого вектора.
         Vector vectorB = new Vector(vectorA);
 
-        System.out.println(vectorB.toString());
+        System.out.println("Вектор B: " + vectorB.toString());
         System.out.println(vectorB.getSize());
 
         // Конструктор с. - заполнение вектора значениями из массива.
-        Vector vectorC = new Vector(new double[]{0.1, 0.2, 0.3});
+        Vector vectorC = new Vector(new double[] {0.1, 0.2, 0.3, 4, 45, 99});
 
-        System.out.println(vectorC.toString());
+        System.out.println("Вектор C: " + vectorC.toString());
         System.out.println(vectorC.getSize());
 
         // Конструктор d. - заполнение вектора значениями из массива.
-        double[] array = new double[]{1, 2, 3, 4.32, 5};
+        double[] array = new double[] {1, 2, 3, 4.32, 5};
 
-        Vector vector3 = new Vector(7, array);
-        System.out.println(vector3);
+        Vector vectorD = new Vector(3, array);
+        System.out.println("Вектор D: " + vectorD);
+
+        // Добавление вектора.
+        System.out.println("Вектор C: " + vectorC.toString());
+        System.out.println("+");
+        System.out.println("Вектор D: " + vectorD);
+        System.out.println("=");
+
+        vectorC.addVector(vectorD);
+        System.out.println("Вектор C: " + vectorC.toString());
+
+        // Умножение вектора на скаляр.
+        vectorC.multiplicationByScalar(10);
+        System.out.println("Вектор C: " + vectorC.toString());
+
+        // Разворот вектора. Умножение на -1.
+        vectorC.reverseVector();
+        System.out.println("Вектор C: " + vectorC.toString());
+
+         */
     }
 }
