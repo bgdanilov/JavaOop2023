745f88e5bd2e3c6ba67f4e99a6a66105bfcb7bec
bgdanilov <bdan@yandex.ru>
2023-08-03 12:14:18 +0700

    Shapes: Пробные изменения в main.

--- Shapes/src/ru/bgdanilov/shape/Shape.java
+++ Shapes/src/ru/bgdanilov/shape/Shape.java
@@ -3,6 +3,9 @@ package ru.bgdanilov.shape;
 /** Создаем интерфейс - кнопки-заготовки на пульте без функций.
  * потом каждую кнопку в классе каждой фигуры переопределим.
  */
+
+// Изменения в ветке main на пробу.
+    
 public interface Shape {
     double getHeight();
     double getWidth();
