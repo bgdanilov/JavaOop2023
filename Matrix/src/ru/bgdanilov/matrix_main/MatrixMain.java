2326d9be61fc1b62cc8e02e3e923167cd1f1f348
bgdanilov <bdan@yandex.ru>
2023-08-24 19:32:00 +0700

    Matrix. Выявлены ошибки в Vector. Начало исправления.

--- Matrix/src/ru/bgdanilov/matrix_main/MatrixMain.java
+++ Matrix/src/ru/bgdanilov/matrix_main/MatrixMain.java
@@ -6,8 +6,10 @@ import ru.bgdanilov.vector.Vector;
 public class MatrixMain {
     public static void main(String[] args) {
         // 1.a. Конструктор - создание пустой матрицы размера n*m.
+        System.out.println("1.a. Конструктор - создание пустой матрицы размера n*m.");
         Matrix matrix1a = new Matrix(2, 3);
-        System.out.println("matrix1a : " + matrix1a);
+        System.out.println("matrix1a размером 2 на 3: " + matrix1a);
+        System.out.println();
 
         // 1.c.	Конструктор - создание из Matrix(double[][]) – из двумерного массива.
         double[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
@@ -22,17 +24,27 @@ public class MatrixMain {
         Vector[] vectors = new Vector[]{new Vector(2), new Vector(4)};
         Matrix matrix1d = new Matrix(vectors);
         System.out.println("matrix1d : " + matrix1d);
+        System.out.println();
 
         // 2.b. Метод. Получение и задание вектора-строки по индексу.
+        System.out.println("2.b. Метод. Получение и задание вектора-строки по индексу.");
+        System.out.println("Получение по индексу 2: Исходная матрица matrix1b" + matrix1b);
         Vector vector2b2 = matrix1b.getRow(2);
         System.out.println("vector2b2 : " + vector2b2);
+        System.out.println();
 
         Vector sourceVector = new Vector(new double[] {9, 10, 11});
         matrix1a.setRow(sourceVector, 1);
         System.out.println("matrix1a[0] <-- {9, 10, 11} : " + matrix1a);
+        System.out.println();
 
         // 2.c. Метод. Получение вектора-столбца по индексу.
+        System.out.println("2.c. Метод. Получение вектора-столбца по индексу");
+        System.out.println("Получение по индексу 0: Исходная матрица matrix1a" + matrix1a);
         Vector vector2c = matrix1a.getColumn(1);
         System.out.println(vector2c);
+        System.out.println();
+
+        System.out.println(matrix1c.getColumn(1));
     }
 }
