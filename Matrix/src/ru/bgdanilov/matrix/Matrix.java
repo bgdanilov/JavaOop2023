33d1dc3d3f329c1b3f4cf4a5498b347e0967bd73
bgdanilov <bdan@yandex.ru>
2023-08-25 17:26:11 +0700

    Matrix: 2.d. Метод. Транспонирование матрицы.

--- Matrix/src/ru/bgdanilov/matrix/Matrix.java
+++ Matrix/src/ru/bgdanilov/matrix/Matrix.java
@@ -3,7 +3,7 @@ package ru.bgdanilov.matrix;
 import ru.bgdanilov.vector.Vector;
 
 public class Matrix {
-    private final Vector[] matrix;
+    private Vector[] matrix;
 
     // 1.a. Конструктор - создание пустой матрицы размера n*m.
     public Matrix(int n, int m) {
@@ -12,12 +12,12 @@ public class Matrix {
         }
 
         // Создаем строки матрицы - массив объектов Vector c n-элементами.
-        this.matrix = new Vector[n];
+        matrix = new Vector[n];
 
         // Теперь нужно в каждый элемент массива matrix, поместить вектор с m-элементами.
         for (int i = 0; i < n; i++) {
             // Используем создание конструктор Vector 1.a.
-            this.matrix[i] = new Vector(m);
+            matrix[i] = new Vector(m);
         }
     }
 
@@ -26,18 +26,13 @@ public class Matrix {
         // Получаем количество строк (векторов) в исходной матрице.
         int n = sourceMatrix.getRowsAmount();
 
-        // И тут можно не проверять, наверное? Конструктор выше не даст нам такую матрицу создать.
-        if (n == 0) {
-            throw new IllegalArgumentException("Число строк в матрице должно быть больше нуля!");
-        }
-
         // Создаем новый массив векторов с тем же количеством элементов.
-        this.matrix = new Vector[n];
+        matrix = new Vector[n];
 
         // Заполняем созданный массив новыми векторами,
         // созданными из векторов матрицы переданного объекта sourceMatrix.
         for (int i = 0; i < n; i++) {
-            this.matrix[i] = new Vector(sourceMatrix.matrix[i]);
+            matrix[i] = new Vector(sourceMatrix.matrix[i]);
         }
     }
 
@@ -60,36 +55,24 @@ public class Matrix {
         }
 
         // Создаем новый массив векторов с тем же количеством элементов.
-        this.matrix = new Vector[n];
+        matrix = new Vector[n];
 
         for (int i = 0; i < n; i++) {
             // Используем 1.d. Конструктор - заполнение вектора значениями из массива.
-            this.matrix[i] = new Vector(m, sourceArray[i]);
+            matrix[i] = new Vector(m, sourceArray[i]);
         }
     }
 
     // 1.d. Конструктор	Matrix(Vector[]) – из массива векторов-строк.
     public Matrix(Vector[] sourceVectors) {
         int n = sourceVectors.length;  // Количество векторов в массиве.
-
-        if (n == 0) {
-            throw new IllegalArgumentException("Число строк в матрице должно быть больше нуля!");
-        }
-
         // Создаем новый массив векторов (матрицу) с тем же количеством элементов.
-        this.matrix = new Vector[n];
-
+        matrix = new Vector[n];
         // Ищем максимальный размер среди векторов массива.
         int size = 0;
 
         for (Vector vector : sourceVectors) {
             int temp = vector.getSize();
-
-            // А может не стоит тут проверять? Мы же вектор через конструктор создаем,
-            // и размером ноль конструктор нам не даст сделать.
-            if (temp == 0) {
-                throw new IllegalArgumentException("Размер вектора должен быть больше нуля!");
-            }
 
             if (temp > size) {
                 size = temp;
@@ -98,12 +81,12 @@ public class Matrix {
 
         // Заполняем матрицу новыми векторами, созданными из исходных векторов.
         for (int i = 0; i < n; i++) {
-            this.matrix[i] = new Vector(sourceVectors[i]);
+            matrix[i] = new Vector(sourceVectors[i]);
 
             // Проверяем на совпадение с максимальным размером, при необходимости
             // дополняем нулями при помощи дополнения временного нового вектора размера size.
-            if (this.matrix[i].getSize() < size) {
-                this.matrix[i].add(new Vector(size));
+            if (matrix[i].getSize() < size) {
+                matrix[i].add(new Vector(size));
             }
         }
     }
@@ -119,13 +102,13 @@ public class Matrix {
 
     // 2.b. Метод. Получение и задание вектора-строки по индексу.
     public Vector getRow(int index) {
-        int n = this.getRowsAmount();
+        int n = getRowsAmount();
 
         if (index < 0 || index > n - 1) {
             throw new IllegalArgumentException("Неверный индекс.");
         }
 
-        return new Vector(this.matrix[index]);
+        return new Vector(matrix[index]);
     }
 
     public void setRow(Vector sourceVector, int index) {
@@ -133,7 +116,7 @@ public class Matrix {
         int m = this.getColumnsAmount();
 
         // index считается от 0, а n - от 1.
-        if (sourceVector.getSize() > m || (index < 0 || index > n - 1 )) {
+        if (sourceVector.getSize() > m || (index < 0 || index > n - 1)) {
             throw new IllegalArgumentException("Векторы не совпадают по размеру или неверный индекс.");
         }
 
@@ -144,37 +127,49 @@ public class Matrix {
 
     // 2.c. Метод. Получение вектора-столбца по индексу.
     public Vector getColumn(int index) {
-        int n = this.getRowsAmount();
+        int n = getRowsAmount();
+        int m = getColumnsAmount();
 
-        if (index < 0 || index > n - 1) {
+        if (index < 0 || index > m - 1) {
             throw new IllegalArgumentException("Неверный индекс.");
         }
 
         Vector outputVector = new Vector(n);
 
         for (int i = 0; i < n; i++) {
-            outputVector.setComponent(i, this.matrix[i].getComponent(index));
+            outputVector.setComponent(i, matrix[i].getComponent(index));
         }
 
         return outputVector;
     }
 
     // 2.d. Метод. Транспонирование матрицы.
+    public void transpose() {
+        int m = getColumnsAmount(); // 3;
 
+        Vector[] vector = new Vector[m];
 
+        for (int i = 0; i < m; i++) {
+            vector[i] = getColumn(i);
+        }
+
+        matrix = vector;
+    }
 
     // 2.g.	Метод toString определить так,
     // чтобы результат получался в таком виде: {{1, 2}, {2, 3}}
     @Override
     public String toString() {
-        int n = this.matrix.length;
-        StringBuilder sb = new StringBuilder().append("{ ").append(this.matrix[0]);
+        StringBuilder sb = new StringBuilder().append("{");
+        int n = matrix.length;
 
-        for (int i = 1; i < n; i++) {
-            sb.append(", ").append(matrix[i]);
+        for (int i = 0; i < n - 1; i++) {
+            sb.append(matrix[i]).append(", ");
         }
 
-        return sb.append(" }").toString();
+        sb.append(matrix[n - 1]).append("}");
+
+        return sb.toString();
     }
 }
 
@@ -206,20 +201,15 @@ public class Matrix {
        IDE предложила добавить некую связь. Можно про это по подробнее, что за связь?
        Почему она нужна?
 
-    2. Куда деваются эти временные new Vector ?
-        this.matrix[i].addVector(new Vector(size));
-
-    3. Конструктор 1.d. А может не стоит тут проверять размерность передаваемого вектора?
+    2. Конструктор 1.d. А может не стоит тут проверять размерность передаваемого вектора?
        Мы же вектор через конструктор создаем, и размером ноль конструктор нам не даст сделать.
        Аналогично Конструктор 1.b.
-
-
 
     n - число строк.
     m - число колонок.
  */
 
-/* TODO
+/* TODO вопросы.
     1. Сделать проверку-добивку нулями в отдельную функцию.
     - сделано: improveSize.
         - удалено.
@@ -228,4 +218,5 @@ public class Matrix {
     - вообще, разобраться с ссылочными данными и как они копируются, создаются, где хранятся.
         - все верно, то была ссылка на vectors, а не создание нового объекта.
         - если бы vectors, то и наша новая матрица бы поменялась. Это неправильно.
+    3. Метод 2.b. задание строки: нужно ли стобы влезала строка несовпадающей размерности?
  */
--- Matrix/src/ru/bgdanilov/matrix_main/MatrixMain.java
+++ Matrix/src/ru/bgdanilov/matrix_main/MatrixMain.java
@@ -6,45 +6,70 @@ import ru.bgdanilov.vector.Vector;
 public class MatrixMain {
     public static void main(String[] args) {
         // 1.a. Конструктор - создание пустой матрицы размера n*m.
-        System.out.println("1.a. Конструктор - создание пустой матрицы размера n*m.");
+        System.out.println("1.a. Конструктор - создание пустой матрицы размера 2 * 3.");
         Matrix matrix1a = new Matrix(2, 3);
         System.out.println("matrix1a размером 2 на 3: " + matrix1a);
         System.out.println();
 
         // 1.c.	Конструктор - создание из Matrix(double[][]) – из двумерного массива.
+        System.out.println("1.c.Конструктор - создание из Matrix(double[][]) – из двумерного массива.");
         double[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
         Matrix matrix1c = new Matrix(array);
         System.out.println("matrix1c : " + matrix1c);
+        System.out.println();
 
         // 1.b. Конструктор копирования - Matrix(Matrix).
+        System.out.println("1.b. Конструктор копирования из matrix1c: " + matrix1c);
         Matrix matrix1b = new Matrix(matrix1c);
         System.out.println("matrix1b : " + matrix1b);
+        System.out.println();
 
         // 1.d. Конструктор	Matrix(Vector[]) – из массива векторов-строк.
-        Vector[] vectors = new Vector[]{new Vector(2), new Vector(4)};
+        System.out.println("1.d. Конструктор Matrix(Vector[]) – из массива векторов-строк.");
+        // TODO Тут потенциальная ошибка в конструкторе Vector 1.d.
+        // TODO new Vector(2, new double[]{1, 2, 4}) - создаст {1, 2}
+        // TODO - т.e. обрежет и не будет ругаться.
+        Vector[] vectors = new Vector[]{new Vector(3, new double[]{1, 2, 4}), new Vector(2, new double[]{3, 4}) };
         Matrix matrix1d = new Matrix(vectors);
         System.out.println("matrix1d : " + matrix1d);
         System.out.println();
 
-        // 2.b. Метод. Получение и задание вектора-строки по индексу.
-        System.out.println("2.b. Метод. Получение и задание вектора-строки по индексу.");
-        System.out.println("Получение по индексу 2: Исходная матрица matrix1b" + matrix1b);
-        Vector vector2b2 = matrix1b.getRow(2);
-        System.out.println("vector2b2 : " + vector2b2);
+        System.out.println("Итого, исходные матрицы:");
+        System.out.println("matrix1a: " + matrix1a);
+        System.out.println("matrix1b: " + matrix1b);
+        System.out.println("matrix1c: " + matrix1c);
+        System.out.println("matrix1d: " + matrix1d);
         System.out.println();
 
+        // 2.b. Метод. Получение и задание вектора-строки по индексу.
+        System.out.println("2.b. Метод. Получение и задание вектора-строки по индексу.");
+        System.out.println("     Получение вектора-строки по индексу 2:");
+        System.out.println("     Исходная матрица matrix1b: " + matrix1b);
+        Vector vector2b = matrix1b.getRow(2);
+        System.out.println("     vector2b : " + vector2b);
+        System.out.println();
+
+        System.out.println("     Задание вектора-строки по индексу 1:");
         Vector sourceVector = new Vector(new double[] {9, 10, 11});
+        System.out.println("     Вектор-строка: " + sourceVector);
+        System.out.println("     Исходная матрица matrix1a: " + matrix1a);
         matrix1a.setRow(sourceVector, 1);
-        System.out.println("matrix1a[0] <-- {9, 10, 11} : " + matrix1a);
+        System.out.println("     Результат - matrix1a: " + matrix1a);
         System.out.println();
 
         // 2.c. Метод. Получение вектора-столбца по индексу.
-        System.out.println("2.c. Метод. Получение вектора-столбца по индексу");
-        System.out.println("Получение по индексу 0: Исходная матрица matrix1a" + matrix1a);
-        Vector vector2c = matrix1a.getColumn(1);
-        System.out.println(vector2c);
+        System.out.println("2.c. Метод. Получение вектора-столбца по индексу.");
+        System.out.println("     Получение по индексу 1:");
+        System.out.println("     Исходная матрица matrix1a: " + matrix1a);
+        Vector vector2c = matrix1a.getColumn(2);
+        System.out.println("     Результат - vector2c: " + vector2c);
         System.out.println();
 
-        System.out.println(matrix1c.getColumn(1));
+        // 2.d. Метод. Транспонирование матрицы.
+        System.out.println("2.d. Метод. Транспонирование матрицы.");
+        System.out.println("     Исходная матрица matrix1d: " + matrix1d);
+        matrix1d.transpose();
+        System.out.println("     Результат: " + matrix1d);
+        System.out.println();
     }
 }
