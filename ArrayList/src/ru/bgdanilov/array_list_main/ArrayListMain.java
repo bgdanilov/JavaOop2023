6c0d31c844185bf0fa75d2bdbdc5a6a523d6b5e3
borisdanilov <bdan@yandex.ru>
2023-08-28 14:38:58 +0700

    ArrayList: Созданы модули и пакеты.

---
+++ ArrayList/src/ru/bgdanilov/array_list_main/ArrayListMain.java
@@ -0,0 +1,10 @@
+package ru.bgdanilov.array_list_main;
+
+import ru.bgdanilov.array_list.ArrayList;
+
+
+public class ArrayListMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
