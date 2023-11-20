f6dd602fd4c86dfe50bc9d499c3e6814c2f0a17b
bgdanilov <bdan@yandex.ru>
2023-11-20 10:48:09 +0700

    Исправления от 08.11.2023 готовы.

--- Temperature/src/ru/bgdanilov/temperature/view/DesktopInputFieldOutputs.java
+++ Temperature/src/ru/bgdanilov/temperature/view/DesktopInputFieldOutputs.java
@@ -12,57 +12,56 @@ import java.util.List;
 
 import static javax.swing.JOptionPane.showMessageDialog;
 
-public class Desktop {
+public class DesktopInputFieldOutputs {
     private final ControllerInterface controller;
 
-    public Desktop(ControllerInterface controller) {
+    private boolean isPressed;
+    private double inputTemperature;
+
+    public DesktopInputFieldOutputs(ControllerInterface controller) {
         this.controller = controller;
     }
 
     public void execute() {
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Конвертер температур");
+            frame.getRootPane().setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
             frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.getRootPane().setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
 
             // В Mac иконка не появляется, а в Win появляется.
             Image icon = Toolkit.getDefaultToolkit().getImage("Temperature/iConvertIcon.png");
             frame.setIconImage(icon);
 
-            // Получаем коллекцию объектов шкал температур.
+            // header, textField.
+            JLabel header = new JLabel("Введите температуру:");
+            JTextField textField = new JTextField("0", 13);
+
             List<ScaleInterface> temperatureScales = controller.getTemperatureScales();
 
-            // Исходные данные.
-            JLabel inputTemperatureLabel = new JLabel("Исходная температура:");
-            JTextField inputTemperatureField = new JTextField("0", 13);
-
+            // inputScaleComboBox, toText, outputScaleComboBox, buttons.
             JComboBox<ScaleInterface> inputScaleComboBox = new JComboBox<>();
             fillComboBoxByTemperatureScales(inputScaleComboBox, temperatureScales);
 
-            // Результирующие данные.
-            JLabel outputTemperatureLabel = new JLabel(" Результирующая температура:");
-            JTextField outputTemperatureField = new JTextField("0", 13);
-            outputTemperatureField.setEditable(false);
+            JLabel toText = new JLabel("конвертировать в:");
 
             JComboBox<ScaleInterface> outputScaleComboBox = new JComboBox<>();
             fillComboBoxByTemperatureScales(outputScaleComboBox, temperatureScales);
 
-            // Кнопки.
             JButton convertButton = new JButton("Конвертировать");
-            JButton resetButton = new JButton("Сброс");
+            JButton resetButton = new JButton("Назад / Сброс");
 
             // Создаем менеджер расположения компонентов.
             GridBagLayout bagLayout = new GridBagLayout();
-            frame.setLayout(bagLayout);
             GridBagConstraints c = new GridBagConstraints();
+            frame.setLayout(bagLayout);
 
-            // Исходные значения сетки.
-            c.gridx = 0; // Координата по x.
-            c.gridy = 0; // Координата по y.
-            // c.gridwidth = 1; // Количество занимаемых столбцов.
+            // header.
+            c.gridx = 1; // Номер столбца.
+            c.gridy = GridBagConstraints.RELATIVE; // Инкремент номера строк автоматически.
             c.gridheight = 1; // Количество занимаемых строк.
-            // c.anchor = GridBagConstraints.NORTHWEST; // Выравнивание по краю.
+            c.gridwidth = 1; // Количество занимаемых столбцов.
+            c.anchor = GridBagConstraints.NORTH; // Выравнивание по краю.
             c.fill = GridBagConstraints.NONE; // Заполнение по доступному пространству.
             c.insets = new Insets(0, 0, 5, 0); // Margin CSS.
             c.ipadx = 0; // Дополнительный отступ.
@@ -70,65 +69,32 @@ public class Desktop {
             c.weightx = 0.0; // Распределение доступной ширины на количество элементов
             c.weighty = 0.0; // Распределение доступной высоты на количество элементов.
 
-            // inputTemperatureLabel.
-            c.gridwidth = 2;
-            c.anchor = GridBagConstraints.CENTER;
-            bagLayout.setConstraints(inputTemperatureLabel, c);
-            frame.add(inputTemperatureLabel);
+            bagLayout.setConstraints(header, c);
+            frame.add(header);
 
-            // Приведение к измененных стилей к исходным.
-            c.gridwidth = 1;
-            c.anchor = GridBagConstraints.NORTHWEST;
-
-            // inputTemperatureField.
-            c.gridx = 0;
-            c.gridy = 1;
-            bagLayout.setConstraints(inputTemperatureField, c);
-            frame.add(inputTemperatureField);
+            // textField.
+            bagLayout.setConstraints(textField, c);
+            frame.add(textField);
 
             // inputScaleComboBox.
-            c.gridx = 1;
-            c.gridy = 1;
             bagLayout.setConstraints(inputScaleComboBox, c);
             frame.add(inputScaleComboBox);
 
-            // outputTemperatureLabel.
-            c.gridx = 0;
-            c.gridy = 2;
-            c.gridwidth = 2;
-            c.anchor = GridBagConstraints.CENTER;
-            bagLayout.setConstraints(outputTemperatureLabel, c);
-            frame.add(outputTemperatureLabel);
-
-            // Приведение к измененных стилей к исходным.
-            c.gridwidth = 1;
-            c.anchor = GridBagConstraints.NORTHWEST;
-
-            // outputTemperatureField
-            c.gridx = 0;
-            c.gridy = 3;
-            bagLayout.setConstraints(outputTemperatureField, c);
-            frame.add(outputTemperatureField);
+            // toText.
+            bagLayout.setConstraints(toText, c);
+            frame.add(toText);
 
             // outputScaleComboBox.
-            c.gridx = 1;
-            c.gridy = 3;
             bagLayout.setConstraints(outputScaleComboBox, c);
             frame.add(outputScaleComboBox);
 
             // resetButton.
-            c.gridx = 0;
-            c.gridy = 4;
-            c.gridwidth = 2;
-            c.anchor = GridBagConstraints.CENTER;
-            c.insets = new Insets(0, 0, 30, 0);
+            c.insets = new Insets(0, 0, 30, 0); //  отступы
             bagLayout.setConstraints(resetButton, c);
             frame.add(resetButton);
 
             // convertButton.
-            c.gridx = 0;
-            c.gridy = 5;
-            c.insets = new Insets(0, 0, 0, 0);
+            c.insets = new Insets(0, 0, 0, 0); //  отступы
             bagLayout.setConstraints(convertButton, c);
             frame.add(convertButton);
 
@@ -137,19 +103,22 @@ public class Desktop {
             frame.setVisible(true);
 
             // Ограничение количества вводимых символов.
-            inputTemperatureField.addKeyListener(new KeyAdapter() {
+            textField.addKeyListener(new KeyAdapter() {
                 @Override
                 public void keyTyped(KeyEvent event) {
                     super.keyTyped(event);
-                    if (inputTemperatureField.getText().length() >= 18) {
+                    if (textField.getText().length() >= 18) {
                         event.consume();
                     }
                 }
             });
 
+            isPressed = false;
+            inputTemperature = 0;
+
             convertButton.addActionListener(event -> {
                 try {
-                    double inputTemperature = Double.parseDouble(inputTemperatureField.getText());
+                    inputTemperature = Double.parseDouble(textField.getText());
                     ScaleInterface inputScale = (ScaleInterface) inputScaleComboBox.getSelectedItem();
                     ScaleInterface outputScale = (ScaleInterface) outputScaleComboBox.getSelectedItem();
 
@@ -157,21 +126,34 @@ public class Desktop {
                             .convertTemperature(inputTemperature, inputScale, outputScale);
 
                     // Формируем сообщение с данными исходной температуры.
-                    String outputTemperatureMessage = getRoundedTemperatureLine(outputTemperature);
+                    assert inputScale != null;
+                    String inputTemperatureMessage = getRoundedTemperatureLine(inputTemperature) + " " + inputScale.key();
+                    String outputTemperatureMessage = getRoundedTemperatureLine(outputTemperature)
+                            + " " + (outputScale != null ? outputScale.key() : ' '); // Был warning на key().
 
-                    outputTemperatureField.setText(outputTemperatureMessage);
+                    textField.setText(inputTemperatureMessage + " = " + outputTemperatureMessage);
+                    textField.setEditable(false);
+                    isPressed = false;
                 } catch (NumberFormatException exception) {
                     showMessageDialog(frame, "Вы должны ввести число.", "Сообщение!", JOptionPane.INFORMATION_MESSAGE);
-                    inputTemperatureLabel.setText("Введите температуру:");
-                    inputTemperatureField.setText("0");
+                    header.setText("Введите температуру:");
+                    textField.setText("0");
+                    textField.setEditable(true);
                 }
             });
 
             resetButton.addActionListener(e -> {
-                inputTemperatureField.setText("0");
-                outputTemperatureField.setText("0");
-                inputScaleComboBox.setSelectedIndex(0);
-                outputScaleComboBox.setSelectedIndex(0);
+                // Только через массив получается.
+                textField.setEditable(true);
+
+                if (!isPressed) {
+                    textField.setText(String.valueOf(inputTemperature));
+                    isPressed = true;
+                } else {
+                    textField.setText("0");
+                    inputScaleComboBox.setSelectedIndex(0);
+                    outputScaleComboBox.setSelectedIndex(0);
+                }
             });
         });
     }
--- Temperature/src/ru/bgdanilov/temperature_main/DesktopOneInputFieldMain.java
+++ Temperature/src/ru/bgdanilov/temperature_main/DesktopOneInputFieldMain.java
@@ -2,7 +2,7 @@ package ru.bgdanilov.temperature_main;
 
 import ru.bgdanilov.temperature.controller.Controller;
 import ru.bgdanilov.temperature.model.*;
-import ru.bgdanilov.temperature.view.DesktopOneInputField;
+import ru.bgdanilov.temperature.view.DesktopInputFieldOutputs;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,7 +18,7 @@ public class DesktopOneInputFieldMain {
 
         Model model = new Model(temperatureScales);
         Controller controller = new Controller(model);
-        DesktopOneInputField userView = new DesktopOneInputField(controller);
+        DesktopInputFieldOutputs userView = new DesktopInputFieldOutputs(controller);
 
         userView.execute();
     }
