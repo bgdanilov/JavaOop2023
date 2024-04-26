3a59bf06bf0aa0f61285922507867814effa56a1
bgdanilov <bdan@yandex.ru>
2024-04-26 17:08:37 +0700

    CSV-v2-keys: готово.

--- CSV/src/ru/bgdanilov/csv_main/CsvMain.java
+++ CSV/src/ru/bgdanilov/csv_main/CsvMain.java
@@ -1,15 +1,32 @@
 package ru.bgdanilov.csv_main;
 
 import ru.bgdanilov.csv.CsvToHtmlBySymbols;
+import ru.bgdanilov.csv.Messages;
+import ru.bgdanilov.csv.Settings;
 
 import java.io.IOException;
 
 public class CsvMain {
     public static void main(String[] args) {
-        CsvToHtmlBySymbols csvParser = new CsvToHtmlBySymbols(',');
+        // Используйте args[] или можно задать аргументы через Edit Configurations.
+        //args = new String[]{"point-comma-sv.csv", "-o", "/Documents", "-s", ";", "-p", "prefix-"};
+        Messages messages = new Messages();
+
+        Settings settings = new Settings(messages);
+        settings.loadSettings(args);
+
+        // В процессе загрузки настроек, записываются ошибки в список messages.
+        // Приступаем к конвертированию, только если все настройки прочитались без ошибок.
+        if (!messages.getMessages().isEmpty()) {
+            System.out.println(messages);
+            return;
+        }
+
+        CsvToHtmlBySymbols csvConverter = new CsvToHtmlBySymbols(settings);
 
         try {
-            csvParser.parseCsvToHtml("CSV/src/files/csv1.csv", "CSV/src/files/html.html");
+            csvConverter.convertCsvToHtml();
+            System.out.println("Файл: " + settings.getUtilityHome() + settings.getCsvFileName() + " успешно обработан.");
         } catch (IOException e) {
             System.out.println("Ошибка: " + e.getMessage());
         }
